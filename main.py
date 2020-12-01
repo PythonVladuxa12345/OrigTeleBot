ae737d74be69d530cbc5e2172566d647cbd1b515
PythonVladuxa12345 <67973022+PythonVladuxa12345@users.noreply.github.com>
2020-12-01 10:14:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-import aiogram
+# -*- coding: utf-8 -*-
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from PIL import Image
+
+bot = Bot(token='1440337804:AAGtyNux9KGyHwbsnqV9vKfPUV93vLDjVhg')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['help'])
+async def process_start_command(msg: types.Message):
+    await msg.reply("Привет!\nБот создан для получения оригинала картинки.\nПришли мне картинку, и я попробую найти оригинал этой картинки!")
+
+
+@dp.message_handler(content_types=['photo'])
+async def process_help_command(msg: types.Message):
+    await bot.send_message(msg.from_user.id, "Красивая картинка")
+    
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    if msg.text.lower() == "привет":
+        await bot.send_message(msg.from_user.id, "И тебе привет!")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
