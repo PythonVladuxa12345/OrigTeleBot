eaba8603c6ad4f33d8cdc65cb70cfb35e55f0942
PythonVladuxa12345 <67973022+PythonVladuxa12345@users.noreply.github.com>
2020-12-02 12:35:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from PIL import Image
 
-bot = Bot(token='1440337804:AAGtyNux0KGyHwbskqV3vKfPUV31vLDjVhg')
+bot = Bot(token='1440337804:AAGtyNux9KGyHwbsnqV9vKfPUV93vLDjVhg')
 dp = Dispatcher(bot)
+path = r"C:\Users\Владик\Desktop\ОригБот"
 
-
-@dp.message_handler(commands=['help'])
+@dp.message_handler(commands=['start'])
 async def process_start_command(msg: types.Message):
-    await msg.reply("Привет!\nБот создан для получения оригинала картинки.\nПришли мне картинку, и я попробую найти оригинал этой картинки!")
+    await msg.reply("Привет!\nНапиши мне что-нибудь!")
 
 
 @dp.message_handler(content_types=['photo'])
 async def process_help_command(msg: types.Message):
     await bot.send_message(msg.from_user.id, "Красивая картинка")
+    await msg.photo[-1].download(path)
+
+
+@dp.message_handler(commands=['image'])
+async def image_get(msg: types.Message):
+    await bot.send_message(msg.from_user.id, "Напишите что нибудь, а я повторю")
     
+        
 @dp.message_handler()
-async def echo_message(msg: types.Message):
+async def hello(msg: types.Message):
     if msg.text.lower() == "привет":
         await bot.send_message(msg.from_user.id, "И тебе привет!")
-
-
 if __name__ == '__main__':
     executor.start_polling(dp)
